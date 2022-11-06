db67992f6e7d3899f1ffab6192aada1adabf8d0b
Ahmad H <ahmadhs.au@gmail.com>
2022-11-07 00:21:03 +1100

    add csv output

--- run.sh
+++ run.sh
@@ -1,5 +1,18 @@
-docker network create back-tier
+# docker network create back-tier
 
-docker compose -f ./docker-compose-monit.yml up -d
+# docker compose -f ./docker-compose-monit.yml up -d
 
-docker compose -f ./docker-compose-evmos.yml up
+# docker compose -f ./docker-compose-evmos.yml up
+
+# sleep 30
+
+
+# docker compose -f ./docker-compose-evmos.yml down
+
+
+result=`~/.go/bin/styx 'tendermint_consensus_total_txs{chain_id="evmos_9000-1", instance="evmos-devnet:26660", job="evmos-tendermint"}'`
+
+
+echo "$result" | tr ";" "," >> test-gen.csv
+
+# docker compose -f ./docker-compose-monit.yml down -d
